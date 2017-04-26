221bf917c7b9cc2cbdc0743acd88bcf0844929da
eddy80310 <eddy_hung9999@hotmail.com>
2017-04-26 16:27:23 +1000

    style added

---
+++ app/api/TodoAPI.jsx
@@ -0,0 +1,54 @@
+var $ = require('jquery');
+
+module.exports = {
+    setTodos: function (todos) {
+        if ($.isArray(todos)) {
+            localStorage.setItem('todos', JSON.stringify(todos))
+            return todos;
+        }
+    },
+    getTodos: function (todos) {
+        var stringTodos = localStorage.getItem('todos');
+        var todos = [];
+        try {
+            todos = JSON.parse(stringTodos);
+        } catch (e) {
+
+        }
+
+        return $.isArray(todos) ? todos : [];
+    },
+    filteredTodos: function (todos, showCompleted, searchText) {
+        var filteredTodos = todos;
+
+        // Filter by show completed
+        filteredTodos = filteredTodos.filter((todo) => {
+            return !todo.completed || showCompleted;
+        })
+
+        // Filter by search text
+        filteredTodos = filteredTodos.filter((todo) => {
+            var text = todo.text.toLowerCase();
+            return searchText.length === 0  || text.indexOf(searchText) > -1;
+        })
+
+        filteredTodos = filteredTodos.filter((todo) => {
+            return !todo.completed || showCompleted;
+        })
+
+        // sort todos with non-completed first
+        filteredTodos.sort((a, b) => {
+            if (!a.completed && b.completed) {
+                return -1;
+            } else if (a.completed && !b.completed) {
+                return 1;
+            } else {
+                return 0;
+            }
+        })
+
+        return filteredTodos;
+    }
+
+
+}
